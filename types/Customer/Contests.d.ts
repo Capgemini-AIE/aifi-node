e6680577d59b14b0262f63ed850fd78fb08d5b7c
Sean Knowles <s@seanknowles.dev>
2022-03-03 17:49:38 +0000

    feat: contest types implemented issue #10

---
+++ types/Customer/Contests.d.ts
@@ -0,0 +1,32 @@
+declare module 'aifi' {
+  namespace Aifi {
+    namespace Customer {
+      interface ContestCreateParams {
+        /**
+         * The order id to be contested
+         */
+        orderId: string;
+
+        /**
+         * The customer's email address.
+         */
+        message: string;
+
+        /**
+         * Items array
+         */
+        items: Aifi.Model.ContestItem[];
+      }
+
+      class ContestsResource {
+        /**
+         * Retrieves an auth token.
+         */
+        create(
+          params: ContestCreateParams,
+          options?: RequestOptions
+        ): Promise<Aifi.Response<Aifi.Model.ContestItem[]>>;
+      }
+    }
+  }
+}
---
+++ types/Models/Contest.d.ts
@@ -0,0 +1,40 @@
+declare module 'aifi' {
+  namespace Aifi {
+    namespace Model {
+      /**
+       * The contest object.
+       */
+      interface ContestItem {
+        /**
+         * TODO: What is rin, clarify??
+         */
+        rin?: string;
+
+        /**
+         * Unique identifier for the product.
+         */
+        productId: string;
+
+        /**
+         *  Item Quantity.
+         */
+        quantity?: number;
+
+        /**
+         *  Original quantity.
+         */
+        originalQuantity?: number;
+
+        /**
+         *  Contested quantity.
+         */
+        contestedQuantity?: number;
+
+        /**
+         *  Reviewed quantity.
+         */
+        reviewedQuantity?: number;
+      }
+    }
+  }
+}

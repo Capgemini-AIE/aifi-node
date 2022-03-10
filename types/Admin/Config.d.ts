6802009908e73579fb496632a5a74ace6617b6c5
Sean Knowles <s@seanknowles.dev>
2022-03-10 09:39:49 +0000

    feat: types config

--- types/Admin/Config.d.ts
+++ types/Admin/Config.d.ts
@@ -1,14 +1,28 @@
 declare module 'aifi' {
   namespace Aifi {
     namespace Admin {
+      interface SuccessResponse {
+        /**
+         * Description of currency configuration
+         */
+        description: string;
+        /**
+         * Current currency code, 'USD', 'CNY', etc...
+         */
+        code: string;
+
+        /**
+         * Current currency symbol, $, £, etc...
+         */
+        symbol: string;
+      }
       class ConfigResource {
         /**
          * Retrieves an auth token.
          */
-        retrieveToken(
-          params: AuthTokenParams,
+        retrief(
           options?: RequestOptions
-        ): Promise<Aifi.Response<Aifi.Admin.Auth>>;
+        ): Promise<Aifi.Response<SuccessResponse>>;
       }
     }
   }

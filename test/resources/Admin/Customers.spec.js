b5629224f7a03926e689f7d7116adb3eb73e9428
Graham Taylor <gtayzlor@gmail.com>
2022-03-10 16:20:42 +0000

    feat: add tests for customers resource (#34)

--- test/resources/Admin/Customers.spec.js
+++ test/resources/Admin/Customers.spec.js
@@ -24,5 +24,80 @@ describe('Admin', () => {
         });
       });
     });
+
+    describe('retrieve', () => {
+      it('Sends the correct request', () => {
+        aifi.admin.customers.retrieve('1');
+        expect(aifi.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/api/admin/v2/customers/1',
+          headers: {},
+          data: {},
+          settings: {},
+        });
+      });
+    });
+
+    describe('list', () => {
+      it('Sends the correct request', () => {
+        aifi.admin.customers.list({externalId: 25});
+        expect(aifi.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/api/admin/v2/customers?externalId=25',
+          headers: {},
+          data: {},
+          settings: {},
+        });
+      });
+    });
+
+    describe('update', () => {
+      it('Sends the correct request', () => {
+        aifi.admin.customers.update('1', {
+          email: 'john.doe5@gmail.com',
+          password: 'new-hard-to-guess',
+          currentPassword: 'hard-to-guess',
+          firstName: 'John',
+          lastName: 'Doe',
+          phone: '15412334363',
+          card: {
+            provider: 'STRIPE',
+            cardToken: 'E67TY8GQ27X',
+            defaultCard: false,
+          },
+        });
+        expect(aifi.LAST_REQUEST).to.deep.equal({
+          method: 'PATCH',
+          url: '/api/admin/v2/customers/1',
+          headers: {},
+          data: {
+            email: 'john.doe5@gmail.com',
+            password: 'new-hard-to-guess',
+            currentPassword: 'hard-to-guess',
+            firstName: 'John',
+            lastName: 'Doe',
+            phone: '15412334363',
+            card: {
+              provider: 'STRIPE',
+              cardToken: 'E67TY8GQ27X',
+              defaultCard: false,
+            },
+          },
+          settings: {},
+        });
+      });
+    });
+    describe('createEntryCode', () => {
+      it('Sends the correct request', () => {
+        aifi.admin.customers.createEntryCode('1');
+        expect(aifi.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/api/admin/v2/customers/1/entry-codes',
+          headers: {},
+          data: {},
+          settings: {},
+        });
+      });
+    });
   });
 });

f15d1b3d2ec897ef1fb2b0ad981c3917ad1acf8a
Sean Knowles <s@seanknowles.dev>
2022-04-21 17:41:38 +0100

    fix: updated test spec

--- test/resources/Customer/Customers.spec.js
+++ test/resources/Customer/Customers.spec.js
@@ -11,14 +11,16 @@ describe('Customer', () => {
         aifi.customer.contests.create({
           productId: '1',
           quantity: 1,
+          items: [],
         });
         expect(aifi.LAST_REQUEST).to.deep.equal({
           method: 'POST',
           url: '/api/customer/v2/contests',
           headers: {},
           data: {
-            productId: 'test@test.com',
+            productId: '1',
             quantity: 1,
+            items: [],
           },
           settings: {},
         });
